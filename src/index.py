d3554c1ea7ec1c3ba34ebc21d8258d6b2798b626
Leonardo Antonio Perez Ardila <lantpard@MacBook-Air-de-Leonardo.local>
2022-06-18 19:16:09 -0500

    add folium map

--- src/index.py
+++ src/index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import folium
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,7 +16,18 @@ def trees():
 
 @app.route('/app')
 def dash():
-    return render_template('app.html')
+    star_coords = (4.430081,-75.2112492)
+
+    folium_map = folium.Map(location=star_coords, zoom_start=13)
+    #folium_map.save('src/templates/map.html')
+    #html_string=folium_map.get_root().render()
+    folium.Marker(
+      location=[4.430081,-75.2112492],
+      popup="leo",
+   ).add_to(folium_map)
+    html_string = folium_map._repr_html_()
+    return render_template('app.html',folium_map=html_string)
+
 
 @app.route('/contact')
 def contact():
